e30f3e5a364d694555895431c8e4cc1df72bbea0
peatear-anthony <peter.a.sunga@gmail.com>
2019-04-25 15:55:56 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 source activate magenta
 
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
+
+git checkout master && git up
+
 jupyter notebook --ip 0.0.0.0 --no-browser --allow-root
 
 
